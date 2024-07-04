6d276010171f95c77c3c69744069fc65b12d2bc0
ronan <ronan.bernard@gmail.com>
2024-07-04 14:45:16 +0200

    proposal of fixing issue #3

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.querySelectorAll('.appliance-btn').forEach(button => {
     });
 });
 
+// ! set longest width for a class
 const getLongestAndSetWidth = cssClass => {
     const group = document.querySelectorAll('.'+cssClass)
     const longest = Math.max(...[...group].map(e => e.offsetWidth))
@@ -12,5 +13,19 @@ const getLongestAndSetWidth = cssClass => {
     return
 }
 
+// ! handle input range values 
+const inputRanges = document.querySelectorAll('.custom-range > input[type="range"]')
+inputRanges.forEach(e => {
+    const output = document.createElement('span')
+    output.classList.add('custom-range-output')
+    output.textContent = e.value+'%'
+    e.parentNode.insertAdjacentElement("afterend", output)
+    e.addEventListener('input', event => output.textContent = event.target.value+"%")
+    e.addEventListener('mouseup', event => {/* action quand on lache la souris de l'input */})
+    e.addEventListener('touchend', event => {/* action quand on lache le doigt de l'input */})
+    return
+})
+
+// * init
 getLongestAndSetWidth('appliance-title')
 window.addEventListener('resize', getLongestAndSetWidth('appliance-title'))
