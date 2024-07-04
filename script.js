ba4e3b9be954e1cb25b7feb15637ecab7de4e4cd
Ronan BERNARD <ronan.bernard@yrnet.com>
2024-07-04 10:20:36 +0200

    issue #2

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ document.querySelectorAll('.appliance-btn').forEach(button => {
         window.open(url, '_blank');
     });
 });
+
+const getLongestAndSetWidth = cssClass => {
+    const group = document.querySelectorAll('.'+cssClass)
+    const longest = Math.max(...[...group].map(e => e.offsetWidth))
+    group.forEach(e => e.style.width = longest+'px')
+    return
+}
+
+getLongestAndSetWidth('appliance-title')
+window.addEventListener('resize', getLongestAndSetWidth('appliance-title'))
